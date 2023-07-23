7f093fe93b012c9734c41561d3387bd9f9113fb3
agnij-dutta <126397667+agnij-dutta@users.noreply.github.com>
2023-07-23 15:36:49 +0530

    Update mode_exe.py

--- Application/model_exe.py
+++ Application/model_exe.py
@@ -1,13 +1,27 @@
 import pickle
 model = pickle.load(open(r'Application/resources/trained_model.sav', 'rb'))
 def execute(emails, model):
+
     '''emails = [str]
     It is reccomended to only pass 1 email per execution for readabitly and easier debugging experience'''
+
     prediction = model.predict(emails)
+    pred_percent = model.predict_proba(emails)
+
     res = "Cannot be determined"
-    for p in prediction:
+    percent = "Cannot be determined"
+
+    for ind in range(len(list(prediction))):
+        p = list(prediction)[ind]
+
         if p == 'spam':
             res = "It is a spam"
-        else:
+            percent = pred_percent[ind][1]
+            print(res, f'Probability: {percent}')
+            continue
+
+        elif p == 'ham':
             res = "It is not a spam"
-    return res
+            percent = pred_percent[ind][1]
+            print(res, f'Probability: {percent}')
+            continue
